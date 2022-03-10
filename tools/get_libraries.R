01f8b7d7e6fb6ae3b16c5630428d6869b87a5d6c
jose <jcmc@fe.up.pt>
2022-03-10 17:28:05 +0000

    Local install.

--- tools/get_libraries.R
+++ tools/get_libraries.R
@@ -1,16 +1,23 @@
 # R repository
 repository="http://cran.us.r-project.org"
+# Local lib
+platform <- R.Version()$'platform'
+version  <- paste(R.Version()[c('major', 'minor')], collapse='.')
+library  <- paste('~/R/', platform, '-library/', version, sep='')
+if (!file.exists(library)) {
+  dir.create(library, showWarnings=TRUE, recursive=TRUE)
+}
 # Install packages
-install.packages('foreach', repos=repository)
-install.packages('doParallel', repos=repository)
-install.packages('extrafont', repos=repository)
-install.packages('stringr', repos=repository)
-install.packages('boot', repos=repository)
-install.packages('data.table', repos=repository)
-install.packages('ggplot2', repos=repository)
-install.packages('RColorBrewer', repos=repository)
-install.packages('reshape2', repos=repository)
-install.packages('hexbin', repos=repository)
+install.packages('foreach', lib=library, repos=repository)
+install.packages('doParallel', lib=library, repos=repository)
+install.packages('extrafont', lib=library, repos=repository)
+install.packages('stringr', lib=library, repos=repository)
+install.packages('boot', lib=library, repos=repository)
+install.packages('data.table', lib=library, repos=repository)
+install.packages('ggplot2', lib=library, repos=repository)
+install.packages('RColorBrewer', lib=library, repos=repository)
+install.packages('reshape2', lib=library, repos=repository)
+install.packages('hexbin', lib=library, repos=repository)
 # Load libraries (aka runtime sanity check)
 library('foreach')
 library('doParallel')
