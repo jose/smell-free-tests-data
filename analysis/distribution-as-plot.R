fe0d5ae53cb562c85177f8f0a97f700ec90a0cc4
jose <jcmc@fe.up.pt>
2022-09-07 18:06:59 +0100

    Adapted script to plot distribution of certain columns.

--- analysis/distribution-as-plot.R
+++ analysis/distribution-as-plot.R
@@ -1,23 +1,23 @@
 # ------------------------------------------------------------------------------
-# This script plots the distribution of smells as boxplot.
+# This script plots the distribution of several columns (e.g., coverage, mutation,
+# size, smelliness) as boxplot.
 #
 # Usage:
 #   Rscript distribution-as-plot.R
 #     <input data file, e.g., ../test-generation/data/generated/data.csv.gz>
-#     <output pdf file, e.g., smell-distribution-as-plot.pdf>
+#     <output pdf file, e.g., distribution-as-plot.pdf>
 # ------------------------------------------------------------------------------
 
 source('../utils/analysis/utils.R')
 
 # Load external packages
-library('ggplot2', lib.loc='../tools/R/x86_64-pc-linux-gnu-library/3.6.3') # FIXME path
-library('reshape2', lib.loc='../tools/R/x86_64-pc-linux-gnu-library/3.6.3') # FIXME path
+library('ggplot2') # install.packages('ggplot2')
 
 # ------------------------------------------------------------------------- Args
 
 args = commandArgs(trailingOnly=TRUE)
 if (length(args) != 2) {
-  stop('USAGE: Rscript distribution-as-plot.R <input data file, e.g., ../test-generation/data/generated/data.csv.gz> <output pdf file, e.g., smell-distribution-as-plot.pdf>')
+  stop('USAGE: Rscript distribution-as-plot.R <input data file, e.g., ../test-generation/data/generated/data.csv.gz> <output pdf file, e.g., distribution-as-plot.pdf>')
 }
 
 # Args
@@ -26,37 +26,54 @@ OUTPUT_FILE <- args[2]
 
 # ------------------------------------------------------------------------- Main
 
+COLUMNS_TO_ANALYZE <- c('Size', 'OverallCoverage', 'MutationScore', 'Smelliness') # TODO anything else
+RELATIVE_COLUMNS_TO_ANALYZE <- c('Size', 'RelativeOverallCoverage', 'RelativeMutationScore', 'RelativeSmelliness') # TODO anything else
+
+# Smells to be considered/analyzed, i.e., the ones that could be optimized as a
+# secondary criteria
+smells <- c(
+  'TestSmellEagerTest',
+  'TestSmellIndirectTesting',
+  'TestSmellObscureInlineSetup',
+  'TestSmellOverreferencing',
+  'TestSmellRottenGreenTests',
+  'TestSmellVerboseTest'
+)
+relative_smells <- paste0('Relative', smells) # Prefix smell metrics names
+
 # Load data
-df                     <- load_TABLE(INPUT_FILE)
-# Collect smelly columns
-all_smell_columns      <- grep(pattern='^TestSmell', x=colnames(df), value=TRUE)
-timeline_smell_columns <- grep(pattern='^TestSmell.*Timeline_T', x=colnames(df), value=TRUE)
-smell_columns          <- setdiff(all_smell_columns, timeline_smell_columns)
-# Select relevant columns for this script
-df                     <- df[ , which(colnames(df) %in% c('configuration_id', 'group_id', 'TARGET_CLASS', smell_columns)) ]
-# Aggregate data
-df                     <- aggregate(x=as.formula(paste('cbind(', paste(smell_columns, collapse=','), ') ~ configuration_id + group_id + TARGET_CLASS', sep=' ')), data=df, FUN=mean)
-# Convert dataframe from column-based to row-based to ease plotting
-df                     <- reshape2::melt(df, id.vars=c('configuration_id', 'group_id', 'TARGET_CLASS'))
+df <- load_data(INPUT_FILE, smells)
+# Compute smelliness of each test
+df <- compute_smelliness(df, smells)
+# Aggregate `df` so that we have coverage, mutation score, and smelliness values per configuration, target class, and random seed
+# Note that at this point `df` is at test case level
+df <- aggregate(as.formula(paste0('cbind(', paste0(c(COLUMNS_TO_ANALYZE, smells), collapse=','), ') ~ configuration_id + TARGET_CLASS + Random_Seed')), data=df, FUN=mean)
+# Compute relative OverallCoverage, MutationScore, and Smelliness
+df <- compute_relativeness(df, c('OverallCoverage'))
+df <- compute_relativeness(df, c('MutationScore'))
+df <- compute_relativeness(df, c(smells, 'Smelliness'))
+# Aggregate `df` so that we have coverage, mutation score, and smelliness values per configuration, target class
+df <- aggregate(as.formula(paste0('cbind(', paste0(RELATIVE_COLUMNS_TO_ANALYZE, collapse=','), ') ~ configuration_id + TARGET_CLASS')), data=df, FUN=mean)
 # Pretty configurations' names
-df$'configuration_id'  <- sapply(df$'configuration_id', pretty_configuration_id)
+df$'configuration_id' <- sapply(df$'configuration_id', pretty_configuration_id_as_abbreviation)
+print(head(df)) # debug
+print(summary(df)) # debug
 
 # Remove any existing output file and create a new one
 unlink(OUTPUT_FILE)
-pdf(file=OUTPUT_FILE, family='Helvetica', width=12, height=7)
+pdf(file=OUTPUT_FILE, family='Helvetica', width=7, height=4)
 # Add a cover page to the output file
 plot_label('Distributions as boxplot')
 
 #
 # Create boxplot
 #
-
-plot_it <- function(df, facets=FALSE) {
+plot_it <- function(df, y_var) {
   # Basic boxplot with colors by groups
-  p <- ggplot(df, aes(x=variable, y=value), fill=variable) + geom_violin() + geom_boxplot(width=0.25)
+  p <- ggplot(df, aes(x=configuration_id, y=get(y_var), fill=configuration_id)) + geom_violin() + geom_boxplot(width=0.25)
   # Change x axis label
   p <- p + scale_x_discrete(name='')
-  # Change y axis label and set its scale
+  # Change y axis label
   p <- p + scale_y_continuous(name='')
   # # Use grey scale color palette
   # p <- p + scale_fill_grey()
@@ -69,20 +86,15 @@ plot_it <- function(df, facets=FALSE) {
   )
   # Make it horizontal
   p <- p + coord_flip()
-  if (facets) {
-    # Create facets, one per configuration
-    p <- p + facet_wrap(~ configuration_id, ncol=2)
-  }
   # Add mean points
   p <- p + stat_summary(fun=mean, geom='point', shape=8, size=2, fill='black', color='black')
   # Print it
   print(p)
 }
 
-# One configuration per page
-for (configuration_id in unique(df$'configuration_id')) {
-  plot_label(paste('Optimized\n', configuration_id, sep=''))
-  plot_it(df[df$'configuration_id' == configuration_id, ])
+for (column in RELATIVE_COLUMNS_TO_ANALYZE) {
+  plot_label(column)
+  plot_it(df, column)
 }
 
 # Close output file
