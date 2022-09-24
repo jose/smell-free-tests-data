9892c8d19c9f4524497af616119ea810d49c36a0
jose <jcmc@fe.up.pt>
2022-09-24 11:48:50 +0100

    Fixed (once more) computation of relative smelliness. Adapted script to print out raw and relative values.

--- analysis/average-data-as-table.R
+++ analysis/average-data-as-table.R
@@ -44,24 +44,24 @@ df <- load_data(INPUT_FILE, smells)
 # Remove EvoSuite's default
 df <- df[df$'configuration_id' %!in% c('verbose-test'), ] # FIXME should not even be in the CSV file
 
-# Revert a normalized value to its non-normalized value
-df <- compute_non_normalized_values(df, smells)
+# Compute smelliness of each test case
+df <- compute_smelliness(df, smells, column_name='Smelliness')
 
 # Aggregate `df` so that we have average coverage, mutation score, and smell values per configuration, target class, and random seed
 # Note that after the following line, `df` is at test suite level
-df <- aggregate(as.formula(paste0('cbind(Size, Length, OverallCoverage, MutationScore, ', paste0(smells, collapse=','), ') ~ configuration_id + group_id + TARGET_CLASS + Random_Seed')), data=df, FUN=mean)
+df <- aggregate(as.formula(paste0('cbind(Size, Length, OverallCoverage, MutationScore, Smelliness, ', paste0(smells, collapse=','), ') ~ configuration_id + group_id + TARGET_CLASS + Random_Seed')), data=df, FUN=mean)
 
-# Compute relative OverallCoverage, MutationScore, and all smells
+# Compute relative value of each smell
+df <- compute_relativeness(df, smells)
+# Compute relative smelliness of each test suite
+df <- compute_relativeness(df, c('Smelliness'))
+# Compute relative OverallCoverage and MutationScore of each test suite
 df <- compute_relativeness(df, c('OverallCoverage'))
 df <- compute_relativeness(df, c('MutationScore'))
-df <- compute_relativeness(df, c(smells))
-
-# Compute smelliness of each test suite
-df <- compute_smelliness(df, relative_smells, column_name='RelativeSmelliness')
 
 # Aggregate `df` so that we have average coverage, mutation score, smelliness, and smell values per configuration and target class
 # Note: basically average at target class level
-df <- aggregate(as.formula(paste0('cbind(Size, Length, RelativeOverallCoverage, RelativeMutationScore, RelativeSmelliness, ', paste0(relative_smells, collapse=','), ') ~ configuration_id + group_id + TARGET_CLASS')), data=df, FUN=mean)
+df <- aggregate(as.formula(paste0('cbind(Size, Length, OverallCoverage, RelativeOverallCoverage, MutationScore, RelativeMutationScore, Smelliness, RelativeSmelliness, ', paste0(smells, collapse=','), ', ', paste0(relative_smells, collapse=','), ') ~ configuration_id + group_id + TARGET_CLASS')), data=df, FUN=mean)
 
 # Set and init tex file
 unlink(OUTPUT_TEX_FILE)
@@ -70,51 +70,59 @@ sink(OUTPUT_TEX_FILE, append=FALSE, split=TRUE)
 cat('\\begin{tabular}{@{\\extracolsep{\\fill}} l rr rrr rrr ', paste0(replicate(length(smells), 'r'), collapse=''), ' rrr } \\toprule\n', sep='')
 cat('              &           &            & \\multicolumn{3}{c}{Coverage}                                                              & \\multicolumn{3}{c}{Mutation}                                                              & \\multicolumn{', length(smells), '}{c}{} & \\multicolumn{3}{c}{Smelliness} \\\\\n', sep='')
 cat('Configuration & \\# Tests & \\# Length & \\multicolumn{1}{c}{$\\bar{x}$} & \\multicolumn{1}{c}{$\\sigma$} & \\multicolumn{1}{c}{CI} & \\multicolumn{1}{c}{$\\bar{x}$} & \\multicolumn{1}{c}{$\\sigma$} & \\multicolumn{1}{c}{CI}', sep='')
-for (smell in relative_smells) {
-  cat(' & \\multicolumn{1}{c}{', gsub("^RelativeTestSmell", '', smell), '}', sep='')
+for (smell in smells) {
+  cat(' & \\multicolumn{1}{c}{', gsub("^TestSmell", '', smell), '}', sep='')
 }
 cat(' & \\multicolumn{1}{c}{$\\bar{x}$} & \\multicolumn{1}{c}{$\\sigma$} & \\multicolumn{1}{c}{CI} \\\\\n', sep='')
-cat('\\midrule\n', sep='')
 
 # Body
-for (configuration_id in unique(df$'configuration_id')) {
-  mask <- df$'configuration_id' == configuration_id
-  cat(pretty_configuration_id_as_abbreviation(configuration_id), sep='')
+print_body <- function(df, cov, mut, smells, smellines, label) {
+  cat('\\midrule\n', sep='')
+  cat('\\rowcolor{gray!25}\n', sep='')
+  cat('\\multicolumn{18}{c}{\\textbf{\\textit{', label, '}}} \\\\\n', sep='')
 
-  # Number of tests
-  cat(' & ', sprintf('%.0f', round(mean(df$'Size'[mask]), 0)), sep='')
+  for (configuration_id in unique(df$'configuration_id')) {
+    mask <- df$'configuration_id' == configuration_id
+    cat(pretty_configuration_id_as_abbreviation(configuration_id), sep='')
 
-  # Number of statements tests
-  cat(' & ', sprintf('%.0f', round(mean(df$'Length'[mask]), 0)), sep='')
+    # Number of tests
+    cat(' & ', sprintf('%.0f', round(mean(df$'Size'[mask]), 0)), sep='')
 
-  # Coverage
-  ci <- get_ci(df$'RelativeOverallCoverage'[mask])
-  cat(' & ', sprintf('%.2f', round(mean(df$'RelativeOverallCoverage'[mask]), 2)),
-      ' & ', sprintf('%.2f', round(sd(df$'RelativeOverallCoverage'[mask]), 2)),
-      ' & [', sprintf('%.2f', round(ci[1], 2)), ', ', sprintf('%.2f', round(ci[2], 2)), ']',
-      sep='')
+    # Number of statements tests
+    cat(' & ', sprintf('%.0f', round(mean(df$'Length'[mask]), 0)), sep='')
 
-  # Mutation
-  ci <- get_ci(df$'RelativeMutationScore'[mask])
-  cat(' & ', sprintf('%.2f', round(mean(df$'RelativeMutationScore'[mask]), 2)),
-      ' & ', sprintf('%.2f', round(sd(df$'RelativeMutationScore'[mask]), 2)),
-      ' & [', sprintf('%.2f', round(ci[1], 2)), ', ', sprintf('%.2f', round(ci[2], 2)), ']',
-      sep='')
+    # Coverage
+    ci <- get_ci(df[[cov]][mask])
+    cat(' & ', sprintf('%.2f', round(mean(df[[cov]][mask]), 2)),
+        ' & ', sprintf('%.2f', round(sd(df[[cov]][mask]), 2)),
+        ' & [', sprintf('%.2f', round(ci[1], 2)), ', ', sprintf('%.2f', round(ci[2], 2)), ']',
+        sep='')
 
-  # Individual smells
-  for (smell in relative_smells) {
-    cat(' & ', sprintf('%.2f', round(mean(df[[smell]][mask]), 2)), sep='')
+    # Mutation
+    ci <- get_ci(df[[mut]][mask])
+    cat(' & ', sprintf('%.2f', round(mean(df[[mut]][mask]), 2)),
+        ' & ', sprintf('%.2f', round(sd(df[[mut]][mask]), 2)),
+        ' & [', sprintf('%.2f', round(ci[1], 2)), ', ', sprintf('%.2f', round(ci[2], 2)), ']',
+        sep='')
+
+    # Individual smells
+    for (smell in smells) {
+      cat(' & ', sprintf('%.2f', round(mean(df[[smell]][mask]), 2)), sep='')
+    }
+
+    # Smelliness
+    ci <- get_ci(df[[smellines]][mask])
+    cat(' & ', sprintf('%.2f', round(mean(df[[smellines]][mask]), 2)),
+        ' & ', sprintf('%.2f', round(sd(df[[smellines]][mask]), 2)),
+        ' & [', sprintf('%.2f', round(ci[1], 2)), ', ', sprintf('%.2f', round(ci[2], 2)), ']',
+        sep='')
+
+    cat(' \\\\\n', sep='')
   }
-
-  # Smelliness
-  ci <- get_ci(df$'RelativeSmelliness'[mask])
-  cat(' & ', sprintf('%.2f', round(mean(df$'RelativeSmelliness'[mask]), 2)),
-      ' & ', sprintf('%.2f', round(sd(df$'RelativeSmelliness'[mask]), 2)),
-      ' & [', sprintf('%.2f', round(ci[1], 2)), ', ', sprintf('%.2f', round(ci[2], 2)), ']',
-      sep='')
-
-  cat(' \\\\\n', sep='')
 }
+
+print_body(df, 'OverallCoverage', 'MutationScore', smells, 'Smelliness', 'Raw values')
+print_body(df, 'RelativeOverallCoverage', 'RelativeMutationScore', relative_smells, 'RelativeSmelliness', 'Relative values')
 
 # Table's footer
 cat('\\bottomrule', '\n', sep='')
